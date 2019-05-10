57b750518ec0e093e6cd6a0c48183ba34b985072
Tez, Gardner <tezgardner1212@gmail.com>
2019-05-10 14:44:56 -0500

    html and css push

---
+++ Assets/javascript/game.js
@@ -0,0 +1,6 @@
+
+var sportsGreats=["Tom Brady","Joe Montana","Kobe Bryant","Tiger Woods","Michael Jordan"]
+
+ 
+var userText=document.getElementById("user-text");
+userText.textContent = event.key;
