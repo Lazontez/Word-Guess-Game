319c7dfc17bbaca30e0e83dc9e46294c1cb4f241
Tez, Gardner <tezgardner1212@gmail.com>
2019-05-11 17:40:09 -0500

    correct letter goes in currentword array. Early Push

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,6 +1,2 @@
 
-var sportsGreats=["Tom Brady","Joe Montana","Kobe Bryant","Tiger Woods","Michael Jordan"]
 
- 
-var userText=document.getElementById("user-text");
-userText.textContent = event.key;
